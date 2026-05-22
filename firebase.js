451b5262a35d239ac0cfb32e0fa23cb7803adcef
winnerindia <princesharma20062026@gmail.com>
2026-05-22 09:41:48 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Firebase Config
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+console.log("Firebase Connected");
